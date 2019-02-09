522ab172e4c8a9a698e251e4eb7e713564311866
Adithya Sreyaj <adi.sreyaj@gmail.com>
2019-02-09 11:21:01 +0530

    Added changes to buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   { path: 'buttons', component: ButtonsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'header', component: HeaderComponent }
 ];
 
 @NgModule({
